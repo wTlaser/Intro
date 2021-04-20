ce66459ce8a5767432773fbdb736ed32fc8512c7
wTlaser <103103591@student.swin.edu.au>
2021-04-20 14:17:39 +1000

    Task 2

--- Selection_Task/Excercise1/Task2/Program.cs
+++ Selection_Task/Excercise1/Task2/Program.cs
@@ -6,11 +6,24 @@ namespace task2
     {
         static void Main(string[] args)
         {
-            
+            string gender;
+            string age;
 
+            Console.WriteLine("User's Gender");
+            gender = Console.ReadLine();
 
+            Console.WriteLine("User's Age");
+            age = Console.ReadLine();
 
-            
+            int int_age;
+
+            int_age = int.Parse(age);
+
+            if(int_age > 17) {
+                Console.WriteLine(gender + " " + "is an adult");
+            } else {
+                Console.WriteLine(gender + " " + "is not an adult");
+            }
         }
     }
 }
