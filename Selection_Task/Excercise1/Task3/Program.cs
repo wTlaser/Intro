8017b80cd2418f24831824678c7e2cf502956ce0
wTlaser <103103591@student.swin.edu.au>
2021-04-20 14:57:59 +1000

    Task 3

---
+++ Selection_Task/Excercise1/Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string numberone;
+           string numbertwo;
+           
+            Console.WriteLine("Eneter first Number");
+            numberone = Console.ReadLine();
+
+            Console.WriteLine("Eneter second Number");
+            numbertwo = Console.ReadLine();
+
+            int int_numberone;
+            int int_numbertwo;
+
+            int_numberone = int.Parse(numberone);
+            int_numbertwo = int.Parse(numbertwo);
+
+            if(int_numberone + int_numbertwo > 10) {
+                Console.WriteLine("Sum of Numbers is greater than 10");
+            } else if(int_numberone + int_numbertwo == 10) {
+                Console.WriteLine("Sum of Numbers equal to 10");
+            } else {
+                Console.WriteLine("Sum of Numbers is less than 10");
+            }
+        }
+    }
+}
