45c82388dc4964db6b9df4c20294d2035af74e4a
wTlaser <103103591@student.swin.edu.au>
2021-04-20 13:18:56 +1000

    Variables Task Complete

---
+++ Variables_Task/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Variables_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname;
+            string surname;
+            string yob;
+            string cy;
+
+           Console.WriteLine("Enter First Name");
+           firstname = Console.ReadLine();
+
+           Console.WriteLine("Enter Surname");
+           surname = Console.ReadLine();
+
+           Console.WriteLine("Enter studentID");
+           Console.ReadLine();
+
+           Console.WriteLine("Year of birth");
+           yob = Console.ReadLine();
+           
+           Console.WriteLine("Enter Current Year");
+           cy = Console.ReadLine();
+
+            int YOB;
+            int CY; 
+        
+            YOB = int.Parse(yob); 
+            CY = int.Parse(cy);
+
+
+
+           Console.WriteLine(firstname + " " + surname);
+           Console.WriteLine(CY - YOB);
+        }
+    }
+}
