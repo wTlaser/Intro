afa2539d2eb97c81bdfc7944b6318200c11d919c
wTlaser <103103591@student.swin.edu.au>
2021-04-20 14:02:17 +1000

    .

---
+++ Selection_Task/Excercise1/Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Selection_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string age;
+
+            Console.WriteLine("User's Age");
+            age = Console.ReadLine();
+
+            int int_age;
+
+            int_age = int.Parse(age);
+
+
+            if(int_age > 17) {
+                Console.WriteLine("Legally Adult Age");
+            } else { 
+                Console.WriteLine("Legally Not Adult Age");
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ Selection_Task/Excercise1/Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+            
+        }
+    }
+}
