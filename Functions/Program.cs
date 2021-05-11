4515b5a6e25c9166a501643b74f6c49f937f84b8
wTlaser <103103591@student.swin.edu.au>
2021-05-11 13:43:51 +1000

    .

---
+++ Functions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1;
+            Person p2;
+
+            //instantiation
+            p1 = new Person("Kermit the Frog", 1960);
+            p2 = new Person("Miss Piggy", 2020);
+
+            //---------------------------------------------
+
+            Car c1 = new_Car;
+            
+        }
+    }
+}
